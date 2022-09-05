c6a8930257b77bc2b05dfa9cd5e15b592615bfbe
benjamin kisenge <benkisenge03@gmail.com>
2022-09-05 12:53:35 +0200

    Create Index file

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const element = <h1>Hello From Create React App</h1>;
+const root = document.getElementById('root');
+ReactDOM.render(element,root);
