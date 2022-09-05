c0c914ce03403965a6e8df041ba0687513a9e312
benjamin kisenge <benkisenge03@gmail.com>
2022-09-05 18:00:05 +0200

    Handle event to input

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -1,10 +1,31 @@
 import React, {Component} from "react";
 
 class InputTodo extends Component {
+    state = {
+        title: ""
+    }
+    onChange = e => {
+        this.setState({
+            [e.target.name] : e.target.value
+        });
+      };
+    handleSubmit = e => {
+        e.preventDefault();
+        console.log(this.state.title);
+        this.setState ({
+            title: ""
+        }) 
+    }
     render () {
         return (
-            <form>
-                <input type="text" placeholder="Add Todo"/>
+            <form onSubmit={this.handleSubmit}>
+                <input 
+                    type="text" 
+                    placeholder="Add Todo"
+                    name="title"
+                    value={this.state.title}
+                    onChange={this.onChange}
+                />
                 <button>Submit</button>
             </form>
         )
