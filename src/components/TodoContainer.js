cbdd6800d8a9d277b3006855efe2a6b3517373a2
benjamin kisenge <benkisenge03@gmail.com>
2022-09-05 13:01:29 +0200

    Complete container component

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class TodoContainer extends React.Component {
+    render () {
+        return (
+            <div>
+                <h1>Hello from Create React App by Benkis</h1>
+                <p>I am Benjamin Kisenge a web dev</p>
+            </div>
+        )
+    }
+}
+
+export default TodoContainer;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+//Import file
+import TodoContainer from "./components/TodoContainer";
 const element = <h1>Hello From Create React App</h1>;
 const root = document.getElementById('root');
-ReactDOM.render(element,root);
+ReactDOM.render(<TodoContainer/>,root);
